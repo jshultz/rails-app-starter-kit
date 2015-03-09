c297641b80038b37ffe78303ca01f87cca005a4c
Jason Shultz <jasshultz@gmail.com>
2015-03-09 06:52:48 -0600

    forcing a commit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store,
-                                       key: '6P6bwc17Hku:2IuuQAnlF2G|0s0My('
+                                       key: 'I=22&v0Z9ATVp6qr$nQ2/{^6w50S0V'
